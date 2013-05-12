34d92177e3f5fd659bffaedc3befc09c86d38d30
arzzcom <arzz@arzz.com>
2013-05-12 23:23:37 +0900

    포인트모듈 DB스키마 변경 및 관리자 수정

---
+++ module/point/admin/category.inc.php
@@ -0,0 +1,6 @@
+<?php
+$categorys = array(
+	array('category'=>'buy','name'=>'포인트구매내역','icon'=>$_ENV['dir'].'/module/point/images/admin/icon_coins.png'),
+	array('category'=>'payment','name'=>'포인트결제방법','icon'=>$_ENV['dir'].'/module/point/images/admin/icon_money.png')
+);
+?>
