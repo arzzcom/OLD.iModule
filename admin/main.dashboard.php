5192427b4e0f69e20b5649d8b521d8dff7d8982f
arzzcom <arzz@arzz.com>
2013-05-20 22:26:44 +0900

    관리자페이지 대시보드 GitHub 링크추가

--- admin/main.dashboard.php
+++ admin/main.dashboard.php
@@ -7,7 +7,32 @@ var ContentArea = function(viewport) {
 		region:"center",
 		title:"대시보드",
 		layout:"fit",
+		margin:"0 5 0 0",
+		tbar:[
+			new Ext.Button({
+				text:"최신업데이트확인",
+				icon:"<?php echo $_ENV['dir']; ?>/images/admin/icon_github.png",
+				handler:function() {
+					window.open("https://github.com/arzzcom/iModule");
+				}
+			}),
+			new Ext.Button({
+				text:"도움말",
+				icon:"<?php echo $_ENV['dir']; ?>/images/admin/icon_github.png",
+				handler:function() {
+					window.open("https://github.com/arzzcom/iModule/wiki");
+				}
+			}),
+			new Ext.Button({
+				text:"질문/버그신고/이슈페이지",
+				icon:"<?php echo $_ENV['dir']; ?>/images/admin/icon_github.png",
+				handler:function() {
+					window.open("https://github.com/arzzcom/iModule/issues");
+				}
+			})
+		],
 		items:[
+		
 		]
 	});
 };
