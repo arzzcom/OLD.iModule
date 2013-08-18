f5005e5081421d8f693b3203e16e5ed8b5527d0e
arzzcom <arzz@arzz.com>
2013-08-18 12:42:56 +0900

    IDadd auto_increment 옵션버그수정

--- module/database/ModuleDatabase.class.php
+++ module/database/ModuleDatabase.class.php
@@ -38,22 +38,24 @@ class ModuleDatabase extends Module {
 		$table = $this->GetTable(0,$table);
 		$data = $this->mDB->DBfetch($table['name'],$selector,$find,$order,$limit,$table['database']);
 
-		foreach ($data as $field=>$value) {
-			if ($table['field'][$field]['type'] == 'FILE') {
-				if ($value && $value != '0') {
-					$file = $this->mDB->DBfetch($this->table['file'],array('idx','filename','filepath','filesize','filetype','hit'),"where `idx`=$value");
-					$file['filepath'] = $_ENV['path'].$file['filepath'];
-					$file['filedir'] = $file['filetype'] == 'IMG' ? $this->moduleDir.'/exec/ShowImage.do.php?idx='.$file['idx'].'&tno='.$table['idx'] : $_ENV['dir'].$file['filepath'];
-					$file['download'] = $this->moduleDir.'/exec/FileDownload.do.php?idx='.$file['idx'].'&tno='.$table['idx'];
-					$data[$field] = $file;
+		if (is_array($data) == true) {
+			foreach ($data as $field=>$value) {
+				if ($table['field'][$field]['type'] == 'FILE') {
+					if ($value && $value != '0') {
+						$file = $this->mDB->DBfetch($this->table['file'],array('idx','filename','filepath','filesize','filetype','hit'),"where `idx`=$value");
+						$file['filepath'] = $_ENV['path'].$file['filepath'];
+						$file['filedir'] = $file['filetype'] == 'IMG' ? $this->moduleDir.'/exec/ShowImage.do.php?idx='.$file['idx'].'&tno='.$table['idx'] : $_ENV['dir'].$file['filepath'];
+						$file['download'] = $this->moduleDir.'/exec/FileDownload.do.php?idx='.$file['idx'].'&tno='.$table['idx'];
+						$data[$field] = $file;
+					} else {
+						$data[$field] = array('idx'=>0,'filename'=>'','filepath'=>'','filedir'=>'','filesize'=>0,'filetype'=>'ETC','hit'=>0,'download'=>'');
+					}
 				} else {
-					$data[$field] = array('idx'=>0,'filename'=>'','filepath'=>'','filedir'=>'','filesize'=>0,'filetype'=>'ETC','hit'=>0,'download'=>'');
+					if ($table['field'][$field]['type'] == 'HTML') {
+						$value = '<div class="smartOutput">'.str_replace('{$moduleDir}',$this->moduleDir,$value).'</div>';
+					}
+					$data[$field] = $value;
 				}
-			} else {
-				if ($table['field'][$field]['type'] == 'HTML') {
-					$value = '<div class="smartOutput">'.str_replace('{$moduleDir}',$this->moduleDir,$value).'</div>';
-				}
-				$data[$field] = $value;
 			}
 		}
 
