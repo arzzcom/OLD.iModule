3ebee009593e04ae113e3f83d8c4d0a8673d57cf
arzzcom <arzz@arzz.com>
2013-08-24 23:45:28 +0900

    기본스킨제외 삭제

--- module/board/templet/board/launcher/script.js
+++
@@ -1,9 +0,0 @@
-function SearchOptionCheck(id) {
-	var thisKey = document.forms["ModuleBoardSearch"].key.value;
-	var option = id.replace("checkbox_","");
-
-	document.getElementById("checkbox_"+thisKey).className = "checkboxoff";
-	document.getElementById(id).className = "checkboxon";
-
-	document.forms["ModuleBoardSearch"].key.value = option;
-}
