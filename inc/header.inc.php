8b139a9e733a9e5c8e5213c754c12e644647cb14
arzzcom <arzz@arzz.com>
2013-12-19 17:47:34 +0800

    기본헤더 html5 구조화 일부적용

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,6 +1,6 @@
 <?php $_ENV['isHeaderIncluded'] = true; if (isset($_ENV['dir']) == false) REQUIRE_ONCE '../config/default.conf.php'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="ko">
 <head>
 <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 <META http-equiv="X-UA-Compatible" content="IE=8" />
