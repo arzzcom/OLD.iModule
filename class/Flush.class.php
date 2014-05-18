0225cdaa8a89a3ab5e467c78ccb4be76776e6852
arzzcom <arzz@arzz.com>
2014-05-18 18:01:44 +0800

    서버데몬용 추가

--- class/Flush.class.php
+++ class/Flush.class.php
@@ -1,18 +1,24 @@
 <?php
 class Flush {
 	function __construct() {
-		@ini_set('zlib.output_compression',0);
-		@ini_set('implicit_flush',1);
-		@ob_end_clean();
+		if (isset($_SERVER['REMOTE_ADDR']) == true) {
+			@ini_set('zlib.output_compression',0);
+			@ini_set('implicit_flush',1);
+			@ob_end_clean();
+			ob_start('ob_gzhandler');
+			ob_flush();
+			ob_implicit_flush(1);
+		}
 		set_time_limit(0);
-		ob_start('ob_gzhandler');
-		ob_flush();
-		ob_implicit_flush(1);
 	}
 	
 	function flush() {
-		echo str_repeat(' ',1024*64);
-		ob_flush();
+		if (isset($_SERVER['REMOTE_ADDR']) == true) {
+			echo str_repeat(' ',1024*64);
+			ob_flush();
+		} else {
+			flush();
+		}
 	}
 }
 ?>
